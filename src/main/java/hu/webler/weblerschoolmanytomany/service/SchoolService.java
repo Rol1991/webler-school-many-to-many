05087c6fb68b3e0dba32c63f362dbcf71c74f350
Szőke Roland <szokeroland0@gmail.com>
2024-02-28 11:12:02 +0100

    created service layer

---
+++ src/main/java/hu/webler/weblerschoolmanytomany/service/SchoolService.java
@@ -0,0 +1,9 @@
+package hu.webler.weblerschoolmanytomany.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class SchoolService {
+}
