7a71e87efb493b038241307967cfec6eef041c6a
Szőke Roland <szokeroland0@gmail.com>
2024-03-01 10:30:50 +0100

    refactor code: deleted empty lines

--- src/main/java/hu/webler/weblerschoolmanytomany/controller/CourseController.java
+++ src/main/java/hu/webler/weblerschoolmanytomany/controller/CourseController.java
@@ -4,7 +4,6 @@ package hu.webler.weblerschoolmanytomany.controller;
 import hu.webler.weblerschoolmanytomany.entity.Course;
 import hu.webler.weblerschoolmanytomany.service.CourseService;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,7 +15,6 @@ import java.util.List;
 @RestController
 @RequestMapping("/api")
 @RequiredArgsConstructor
-@Slf4j
 public class CourseController {
 
     private final CourseService courseService;
--- src/main/java/hu/webler/weblerschoolmanytomany/controller/StudentController.java
+++ src/main/java/hu/webler/weblerschoolmanytomany/controller/StudentController.java
@@ -3,7 +3,6 @@ package hu.webler.weblerschoolmanytomany.controller;
 import hu.webler.weblerschoolmanytomany.entity.Student;
 import hu.webler.weblerschoolmanytomany.service.StudentService;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,7 +12,6 @@ import java.util.List;
 @RestController
 @RequestMapping("/api")
 @RequiredArgsConstructor
-@Slf4j
 public class StudentController {
 
     private final StudentService studentService;
