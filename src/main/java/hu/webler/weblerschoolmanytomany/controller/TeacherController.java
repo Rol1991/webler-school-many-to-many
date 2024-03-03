1adac4ea05b452a8508d8e6edbb86645cde9b606
Szőke Roland <szokeroland0@gmail.com>
2024-03-03 13:47:05 +0100

    Created REST API for teacher

--- src/main/java/hu/webler/weblerschoolmanytomany/controller/TeacherController.java
+++ src/main/java/hu/webler/weblerschoolmanytomany/controller/TeacherController.java
@@ -3,11 +3,17 @@ package hu.webler.weblerschoolmanytomany.controller;
 import hu.webler.weblerschoolmanytomany.entity.Teacher;
 import hu.webler.weblerschoolmanytomany.service.TeacherService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api")
@@ -16,8 +22,19 @@ public class TeacherController {
 
     private final TeacherService teacherService;
 
-    @GetMapping("/Teacher")
+    @GetMapping("/Teachers")
     public List<Teacher> renderAllTeachers() {
         return teacherService.getAllTeachers();
     }
+
+    @GetMapping("/teachers")
+    public Optional<Teacher> renderTeacherById(Long id) {return teacherService.getTeacherById(id);}
+
+    @PutMapping("/teachers/add")
+    public Teacher addTeacher(Teacher teacher) {return teacherService.addTeacher(teacher);}
+
+    @PostMapping("/teacher/update")
+    public Teacher updateTeacher(@PathVariable Long id, @RequestBody Teacher teacher) {return teacherService.updateTeacher(id, teacher);}
+    @DeleteMapping("/teachers/{id}")
+    public void deleteTeacher(Long id) {teacherService.deleteTeacher(id);}
 }
