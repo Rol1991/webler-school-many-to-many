38d783a85c66f06aae3aa5f8c6a3cd3651ad405a
Szőke Roland <szokeroland0@gmail.com>
2024-03-03 13:42:53 +0100

    Created REST API for Student

--- src/main/java/hu/webler/weblerschoolmanytomany/controller/StudentController.java
+++ src/main/java/hu/webler/weblerschoolmanytomany/controller/StudentController.java
@@ -3,7 +3,12 @@ package hu.webler.weblerschoolmanytomany.controller;
 import hu.webler.weblerschoolmanytomany.entity.Student;
 import hu.webler.weblerschoolmanytomany.service.StudentService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,4 +25,20 @@ public class StudentController {
     public List<Student> renderAllStudents() {
         return studentService.getAllStudents();
     }
+
+    @GetMapping("/students/id")
+    public Student renderStudentById(Long id) {return studentService.findStudentById(id);}
+
+    @PostMapping("students/{id}")
+    public Student postNewStudent(@RequestBody Student student) {
+        return studentService.addNewStudent(student);
+    }
+
+    @PatchMapping("/students/update")
+    public Student patchCourse(@PathVariable Long id,@RequestBody Student student) {
+        return studentService.updateStudent(id, student);
+    }
+
+    @DeleteMapping("/students/{id}")
+    public void deleteStudent(@PathVariable Long id, @RequestBody Student student) {studentService.deleteStudents(id);}
 }
