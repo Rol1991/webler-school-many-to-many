dca90eaf4afd72b299c1bc0fcfb88b1be86638a8
Szőke Roland <szokeroland0@gmail.com>
2024-03-03 13:47:57 +0100

    Created logic for getting all the Schools in SchoolService

--- src/main/java/hu/webler/weblerschoolmanytomany/service/SchoolService.java
+++ src/main/java/hu/webler/weblerschoolmanytomany/service/SchoolService.java
@@ -3,17 +3,18 @@ package hu.webler.weblerschoolmanytomany.service;
 import hu.webler.weblerschoolmanytomany.entity.School;
 import hu.webler.weblerschoolmanytomany.persistence.SchoolRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class SchoolService {
 
     private final SchoolRepository schoolRepository;
 
-    public List<School> getAllSchools() {
-        return schoolRepository.findAll();
-    }
+    public List<School> getSchool() {return schoolRepository.findAll();}
+
 }
