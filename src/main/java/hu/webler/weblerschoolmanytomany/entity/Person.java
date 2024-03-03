254bdcc2cb04357e74591d0b02c0715179941bb0
Szőke Roland <szokeroland0@gmail.com>
2024-03-03 13:44:18 +0100

    refactor code: moved Person from base to entity package

--- src/main/java/hu/webler/weblerschoolmanytomany/entity/Person.java
+++ src/main/java/hu/webler/weblerschoolmanytomany/entity/Person.java
@@ -3,11 +3,13 @@ package hu.webler.weblerschoolmanytomany.entity;
 import hu.webler.weblerschoolmanytomany.entity.base.Identifier;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
+import lombok.Setter;
 
 import java.time.LocalDate;
 
 @MappedSuperclass
 @Getter
+@Setter
 public class Person extends Identifier {
 
     private String firstname;
