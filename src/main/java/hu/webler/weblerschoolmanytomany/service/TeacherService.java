e442ce3ec938a46baa1391f45e04f99342744417
Szőke Roland <szokeroland0@gmail.com>
2024-03-03 13:46:30 +0100

    Created service logic for Teacher

--- src/main/java/hu/webler/weblerschoolmanytomany/service/TeacherService.java
+++ src/main/java/hu/webler/weblerschoolmanytomany/service/TeacherService.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -15,5 +16,29 @@ public class TeacherService {
 
     public List<Teacher> getAllTeachers() {
         return teacherRepository.findAll();
+    }
+
+    public Teacher addTeacher(Teacher teacher) {
+        teacher.setFirstname((teacher.getFirstname()));
+        teacher.setMidName(teacher.getMidName());
+        teacher.setLastName(teacher.getLastName());
+        teacher.setCell(teacher.getCell());
+        teacher.setEmail(teacher.getEmail());
+        teacher.setDateOffBirth(teacher.getDateOffBirth());
+        teacher.setName(teacher.getName());
+        teacher.setCourses(teacher.getCourses());
+        return teacherRepository.save(teacher);}
+
+    public Optional<Teacher> getTeacherById(Long id) {return teacherRepository.findById(id);}
+
+    public Teacher updateTeacher(Long id, Teacher teacher) {
+        Teacher existingTeacher = teacherRepository.getTeacherById(id);
+        existingTeacher.setName(teacher.getName());
+        return teacherRepository.save(teacher);
+    }
+
+    public void deleteTeacher(Long id) {
+        Teacher teacher = teacherRepository.getTeacherById(id);
+        teacherRepository.delete(teacher);
     }
 }
