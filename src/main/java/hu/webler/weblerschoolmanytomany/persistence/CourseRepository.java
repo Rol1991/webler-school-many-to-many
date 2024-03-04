0cf8177ea249bcebaa473c6bff2dc47ec87713e7
Szőke Roland <szokeroland0@gmail.com>
2024-03-04 12:14:55 +0100

    code refactor: Deleted unused imports

--- src/main/java/hu/webler/weblerschoolmanytomany/persistence/CourseRepository.java
+++ src/main/java/hu/webler/weblerschoolmanytomany/persistence/CourseRepository.java
@@ -4,10 +4,7 @@ import hu.webler.weblerschoolmanytomany.entity.Course;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.io.Writer;
 
 @Repository
 public interface CourseRepository extends JpaRepository<Course, Long> {
-
-    Course findCourseById(Long id);
 }
--- src/main/java/hu/webler/weblerschoolmanytomany/persistence/SchoolRepository.java
+++ src/main/java/hu/webler/weblerschoolmanytomany/persistence/SchoolRepository.java
@@ -4,6 +4,8 @@ import hu.webler.weblerschoolmanytomany.entity.School;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface SchoolRepository extends JpaRepository<School, Long> {
 }
