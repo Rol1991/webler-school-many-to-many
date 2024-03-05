f649cbe7d9cc56521615881bae04d6b4e8e7c05b
Szőke Roland <szokeroland0@gmail.com>
2024-03-05 15:18:55 +0100

    Created model package with courseModel and CourseCreateModel

---
+++ src/main/java/hu/webler/weblerschoolmanytomany/model/CourseCreateModel.java
@@ -0,0 +1,20 @@
+package hu.webler.weblerschoolmanytomany.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class CourseCreateModel {
+
+    private String name;
+    private String description;
+    private LocalDate startDate;
+    private LocalDate endDate;
+}
---
+++ src/main/java/hu/webler/weblerschoolmanytomany/model/CourseModel.java
@@ -0,0 +1,21 @@
+package hu.webler.weblerschoolmanytomany.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class CourseModel {
+
+    private Long id;
+    private String name;
+    private String description;
+    private LocalDate startDate;
+    private LocalDate endDate;
+}
