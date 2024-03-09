b515a3d93c89307e08fafae95554da6846b4d825
Szőke Roland <szokeroland0@gmail.com>
2024-03-09 07:13:29 +0100

    Created mapper logic for Teacher, Student, School, Course, models

--- src/main/java/hu/webler/weblerschoolmanytomany/util/Mapper.java
+++ src/main/java/hu/webler/weblerschoolmanytomany/util/Mapper.java
@@ -2,8 +2,17 @@ package hu.webler.weblerschoolmanytomany.util;
 
 
 import hu.webler.weblerschoolmanytomany.entity.Course;
+import hu.webler.weblerschoolmanytomany.entity.School;
+import hu.webler.weblerschoolmanytomany.entity.Student;
+import hu.webler.weblerschoolmanytomany.entity.Teacher;
 import hu.webler.weblerschoolmanytomany.model.CourseCreateModel;
 import hu.webler.weblerschoolmanytomany.model.CourseModel;
+import hu.webler.weblerschoolmanytomany.model.SchoolCreateModel;
+import hu.webler.weblerschoolmanytomany.model.SchoolModel;
+import hu.webler.weblerschoolmanytomany.model.StudentCreateModel;
+import hu.webler.weblerschoolmanytomany.model.StudentModel;
+import hu.webler.weblerschoolmanytomany.model.TeacherCreateModel;
+import hu.webler.weblerschoolmanytomany.model.TeacherModel;
 
 public class Mapper {
 
@@ -14,6 +23,8 @@ public class Mapper {
         model.setDescription(course.getDescription());
         model.setStartDate(course.getStartDate());
         model.setEndDate(course.getEndDate());
+        model.setSchool(course.getSchool());
+        model.setTeacher(course.getTeacher());
         return model;
     }
 
@@ -24,6 +35,62 @@ public class Mapper {
         currentCourse.setStartDate(model.getStartDate());
         currentCourse.setEndDate(model.getEndDate());
         return currentCourse;
+    }
+
+    public static StudentModel mapStudentEntityToStudentModel (Student student) {
+        StudentModel model = new StudentModel();
+        model.setId(student.getId());
+        model.setFirstName(student.getFirstName());
+        model.setMidName(student.getMidName());
+        model.setLastName(student.getLastName());
+        model.setEmail(student.getEmail());
+        model.setCell(student.getCell());
+        return model;
+    }
+
+    public static Student mapStudentCreateModelToStudentEntity(StudentCreateModel model) {
+        Student currentStudent = new Student();
+        currentStudent.setFirstName(model.getFirstName());
+        currentStudent.setMidName(model.getMidName());
+        currentStudent.setLastName(model.getLastName());
+        currentStudent.setEmail(model.getEmail());
+        currentStudent.setCell(model.getCell());
+        currentStudent.setDateOffBirth(model.getDateOffBirth());
+        return model;
+    }
+
+
+
+    public static TeacherModel  mapTeacherEntityToTeacherModel(Teacher teacher) {
+        TeacherModel model = new TeacherModel();
+        model.setId(teacher.getId());
+        model.setName(teacher.getName());
+        model.setCourses(teacher.getCourses());
+        return model;
+    }
+
+    public static Teacher mapTeacherCreateModelToTeacherEntity(TeacherCreateModel model) {
+        Teacher teacher = new Teacher();
+        teacher.setName(model.getName());
+        teacher.setCourses(model.getCourses());
+        return teacher;
+    }
+
+    public static SchoolModel mapSchoolEntityToSchoolModel(School school) {
+        SchoolModel model = new SchoolModel();
+        model.setId(school.getId());
+        model.setName(school.getName());
+        model.setAddress(school.getAddress());
+        model.setCourses(school.getCourses());
+        return model;
+    }
+
+    public static School mapSchoolCreateModelToSchoolEntity(SchoolCreateModel model) {
+        School school = new School();
+        school.setName(model.getName());
+        school.setAddress(model.getAddress());
+        school.setCourses(model.getCourses());
+        return school;
     }
 
     private Mapper() {
